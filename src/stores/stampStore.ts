88af3197c56a39f19651b2837087dc455e577e52
jadrake75 <jadrake75@gmail.com>
2024-04-11 01:03:08 -0400

    Update stampStore.ts
    * Readded the find( ) to prevent the hang due to the super class function issue

--- src/stores/stampStore.ts
+++ src/stores/stampStore.ts
@@ -6,6 +6,7 @@ import { useStore } from 'pinia-generic'
 import type { PiniaStore } from 'pinia-generic'
 import type { BaseModelStore } from '@/stores/baseModelStore'
 import { baseModelStore } from '@/stores/baseModelStore'
+import type { EntityList } from '@/models/entityList'
 
 type StampStoreType = PiniaStore<'stampStore', {}, {}, {}, BaseModelStore<Stamp>>
 
@@ -19,6 +20,33 @@ export const stampStore = useStore<StampStoreType, BaseModelStore<Stamp>>(
       }
     },
     actions: {
+      /**
+       * Find stamps based on the options specified.  Since the stamp store is a filtered store/query and not
+       * typically queried without filter criteria, the baseModelStore find is insufficient currently.
+       * A redesign would be needed to address this fully.
+       *
+       * @override
+       * @param options
+       */
+      async find(options?: {}): Promise<Array<Stamp>> {
+        this.items.list.splice(0, this.items.list.length)
+        this.items.loading = true
+        const data: EntityList<Stamp> = await this.service.find(options)
+        data.items.forEach((e) => {
+          const activeCN = e.catalogueNumbers.find((cn: CatalogueNumber) => {
+            return cn.active
+          })
+          e.activeCatalogueNumber = activeCN
+          this.items.list.push(e)
+          this.items.total = data.total
+        })
+        this.items.loading = false
+        return this.items.list
+      },
+      /**
+       * @override
+       * @param stamp
+       */
       postFind(stamp: Stamp): Stamp {
         const activeCN = stamp.catalogueNumbers.find((cn: CatalogueNumber) => {
           return cn.active
