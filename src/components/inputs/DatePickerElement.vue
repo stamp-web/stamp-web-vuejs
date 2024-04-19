cc3fb06d34ef804287ef0e6f662c47be1806e395
jadrake75 <jadrake75@gmail.com>
2024-04-18 22:07:43 -0400

    attempting to declare the addon in a slot for Date Picker
    To work around CSP error

--- src/components/inputs/DatePickerElement.vue
+++ src/components/inputs/DatePickerElement.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-  import { ref, nextTick, onBeforeUnmount, onMounted } from 'vue'
+  import { ref } from 'vue'
 
   const $props = defineProps({
     name: String,
@@ -21,26 +21,6 @@
   const clearValue = () => {
     $datePicker.value.clear()
   }
-
-  const handleClick = (evt: PointerEvent) => {
-    const clz: string = (evt.target as HTMLElement).className
-    if (clz.includes('sw-icon-calendar')) {
-      openDatePicker()
-    } else if (clz.includes('sw-icon-cancel')) {
-      clearValue()
-    }
-  }
-
-  onBeforeUnmount(() => {
-    /*$datePicker?.value?.$el
-      ?.querySelector('.form-bg-addon')
-      ?.removeEventListener('click', handleClick)*/
-  })
-
-  onMounted(async () => {
-    await nextTick()
-    //$datePicker?.value?.$el?.querySelector('.form-bg-addon')?.addEventListener('click', handleClick)
-  })
 </script>
 <template>
   <DateElement
@@ -49,17 +29,20 @@
     ref="$datePicker"
     size="sm"
     v-model="model"
-    :addons="{
-      after: {
-        template: ` <i class='sw-icon-cancel cursor-pointer text-gray-300 hover:text-current pb-0.5 px-0.5 scale-110'></i>
- <i class='cursor-pointer sw-icon-calendar text-gray-600 hover:text-black pb-0.5 pl-0.5 scale-110'></i>`
-      }
-    }"
     :add-Classes="{
       DatepickerWrapper: {
         calendarContainer: 'max-w-56 w-56'
       }
     }"
     :columns="$props.columns || ''"
+    ><template #addon-after="">
+      <i
+        @click="clearValue"
+        class="sw-icon-cancel cursor-pointer text-gray-300 hover:text-current pb-0.5 px-0.5 scale-110"
+      ></i>
+      <i
+        @click="openDatePicker"
+        class="cursor-pointer sw-icon-calendar text-gray-600 hover:text-black pb-0.5 pl-0.5 scale-110"
+      ></i> </template
   ></DateElement>
 </template>
