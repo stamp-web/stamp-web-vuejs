10392de2c5a10ee0c3946b882575553dcdbdde4f
jadrake75 <jadrake75@gmail.com>
2024-04-18 21:34:50 -0400

    Another test searching for the CSP cause

--- src/components/inputs/DatePickerElement.vue
+++ src/components/inputs/DatePickerElement.vue
@@ -32,14 +32,14 @@
   }
 
   onBeforeUnmount(() => {
-    $datePicker?.value?.$el
+    /*$datePicker?.value?.$el
       ?.querySelector('.form-bg-addon')
-      ?.removeEventListener('click', handleClick)
+      ?.removeEventListener('click', handleClick)*/
   })
 
   onMounted(async () => {
     await nextTick()
-    $datePicker?.value?.$el?.querySelector('.form-bg-addon')?.addEventListener('click', handleClick)
+    //$datePicker?.value?.$el?.querySelector('.form-bg-addon')?.addEventListener('click', handleClick)
   })
 </script>
 <template>
@@ -49,6 +49,12 @@
     ref="$datePicker"
     size="sm"
     v-model="model"
+    :addons="{
+      after: {
+        template: ` <i class='sw-icon-cancel cursor-pointer text-gray-300 hover:text-current pb-0.5 px-0.5 scale-110'></i>
+ <i class='cursor-pointer sw-icon-calendar text-gray-600 hover:text-black pb-0.5 pl-0.5 scale-110'></i>`
+      }
+    }"
     :add-Classes="{
       DatepickerWrapper: {
         calendarContainer: 'max-w-56 w-56'
